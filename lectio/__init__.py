599dd6d6609c8350e66fdf0a5ad73441fdd87dbf
Jeppe Klitgaard <jeppe@dapj.dk>
2015-05-13 21:55:40 +0200

    Version: 0.0.7

--- lectio/__init__.py
+++ lectio/__init__.py
@@ -8,7 +8,7 @@ Currently supplies the Period class, which represents a period of a lectio
 timetable.
 """
 
-__version__ = (0, 0, 6)
+__version__ = (0, 0, 7)
 
 # This is by no means beautiful code - scrapers rarely are.
 
